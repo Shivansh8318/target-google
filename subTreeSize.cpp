ba85a7c2efe85ce1c6ad0dfc250f3b5fa74ef675
Shivansh8318 <105434693+Shivansh8318@users.noreply.github.com>
2023-04-10 04:55:45 +0530

    Create subTreeSize.cpp

---
+++ subTreeSize.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+#define rep(i,m,n) for(int i = m; i < (int)(n); i++)
+
+#define rrep(i,m,n) for(int i = (int)(m) ; i >= n; i--)
+#define all(x) begin(x), end(x)
+#define raint(x) rbegin(x), rend(x)
+
+int n;
+vector<vector<int>> g;
+vector<int> subTreeSz; 
+
+void dfs(int nn,int p){
+    subTreeSz[nn]=1;
+    for(auto x:g[nn]){
+        if(x!=p){
+            dfs(x,nn);
+            subTreeSz[nn]+=subTreeSz[x];
+        }
+    }
+}
+void solve() {
+    cin>>n;
+    g.resize(n+1);
+    subTreeSz.resize(n+1);
+    rep(i,0,n-1){
+        int a,b;
+        cin>>a>>b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    
+    
+  
+ 
+}
+
+ 
+
+signed main(){
+  int t;
+   cin>>t;
+   int count=1;
+   while(t--){
+   solve();
+   cout<<'\n';
+   }
+}
