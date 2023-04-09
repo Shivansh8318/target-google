fc43706e7c87f27ee1efe1439e78db09adcae6f9
Shivansh8318 <105434693+Shivansh8318@users.noreply.github.com>
2023-04-10 04:34:24 +0530

    Update dfsOnTree.cpp

--- dfsOnTree.cpp
+++ dfsOnTree.cpp
@@ -1,4 +1,9 @@
 //just do not visit parent again
+//diameter of a tree-longest path in tree
+//to find diameter-ksis bhi node pe dfs call kro ab jo bhi max dis aaye use dfs call kro,ab jo bhi max dis aaya whi diameter ki length h
+//centre of tree-node at half of diameter
+//centroid-aisi node jise sare subtree ki size n/2 se kam ho,every tree has a centroid
+
 
 void dfs(int node,int par){
   for(auto x:node){
