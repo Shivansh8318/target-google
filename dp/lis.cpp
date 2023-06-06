495b85023b26d20811731682a180d65acb33764b
Shivansh8318 <105434693+Shivansh8318@users.noreply.github.com>
2023-06-06 23:47:16 +0530

    Update lis.cpp

--- dp/lis.cpp
+++ dp/lis.cpp
@@ -1,1 +1,12 @@
-
+vector<int> soln;
+for(auto x:v)
+{
+  if(soln.empty() || x>soln.back()){
+  soln.push_back(x);
+  }
+  else{
+    auto it=lower_bound(all(v),x);
+    *it=v;
+    }
+  cout<<soln.size();
+}
